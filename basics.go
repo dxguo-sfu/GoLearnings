095e7aa66c2ec6de880597aaaea09e79cde3f174
dxguo <dxguo@sfu.ca>
2018-11-11 18:27:51 -0800

    Make basics and structs+methods files

---
+++ basics.go
@@ -0,0 +1,78 @@
+// The package name of this file
+package main
+
+// By convention, the package name is the same as the last element of the import path
+// "factored" import statement below, could also put them on multiple "import x" lines
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a = "hello"    // Type is inferred from the right side
+	b := "world"       // Declare and initialize with :=
+	var c, d string    // Only declare, uninitialized vars will hold 0 value ie. ""
+	numA, numB := 5, 6 // Standard naming convention: camelCase
+
+	// A name is exported if it begins with a capital letter, math.pi will NOT work
+	fmt.Println("Math.Pi = ", math.Pi)
+
+	// Calling a function and using sprintf
+	fmt.Println(fmt.Sprintf("add %d %d = %d", numA, numB, add(numA, numB)))
+
+	// Return tuple
+	c, d = Swap(a, b)
+	fmt.Printf("Swap `%s` and `%s`: `%s` `%s`\n", a, b, c, d)
+
+	numC, numD := split(17)
+	fmt.Printf("Split 17 into %d and %d\n", numC, numD)
+
+	// Type conversion, printing types
+	fmt.Printf("Type of numA: %T\tNow converted: %T\n", numA, float32(numA))
+}
+
+// (x, y int) : only the last param needs to be type defined if they share the same type
+func add(x, y int) int {
+	return x + y
+}
+
+// Swap returns a tuple. Since it is capitalized it will be exported as part of this package
+func Swap(x, y string) (string, string) {
+	return y, x
+}
+
+// Named return values, (x, y int) names the return values so they can be used in the function. 'return' returns all named variables
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+/* 	Setup and Workspace
+
+The Go workspace has 2 folders, bin and src.
+src typically holds many difference source controlled packages, bin holds executables (binaries)
+Go will always look in packages under the $GOPATH variable, which we can see with 'go env GOPATH'
+If we want to write our own packages we must include the Go workspace in the GOPATH
+This can be done by adding `export GOPATH="path/to/go/workspace:$HOME/go"`
+The path/to/go/workspace should have a src and bin folders
+GOPATH is colon separated
+$HOME/go is the default GOPATH
+*/
+
+/*	Imports and Packages
+	A package is module that can be imported.
+	A Go files under the same folder must be part of the same package
+	Therefore, go filenames do not need to be the same as the package name
+*/
+
+/*	Types
+	Basic Types:
+	bool
+	string	int  int8  int16  int32  int64
+	uint uint8 uint16 uint32 uint64 uintptr
+	byte // alias for uint8
+	rune // alias for int32, represents a Unicode code point
+	float32 float64
+	complex64 complex128
+*/
---
+++ structsAndMethods.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	a := Point{x: 1, y: 2}
+	b := Point{3, 4.5}
+	c := NewPoint(5, 6)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	fmt.Println(a.Norm())
+	fmt.Println(b.Norm())
+	fmt.Println(c.Norm())
+}
+
+// Point is a pointer struct with an x and y coordinate
+type Point struct {
+	x float64
+	y float64
+}
+
+// NewPoint creates a new Point struct
+func NewPoint(x, y float64) Point {
+	return Point{x: x, y: y}
+}
+
+// String provides the default print formatting for the Point struct
+func (p Point) String() string {
+	sX := strings.TrimRight(fmt.Sprintf("%f", p.x), "0")
+	sX = strings.TrimRight(sX, ".")
+	sY := strings.TrimRight(fmt.Sprintf("%f", p.y), "0")
+	sY = strings.TrimRight(sY, ".")
+	s := fmt.Sprintf("(%s, %s)", sX, sY)
+	return s
+}
+
+// Norm calculates the Euclidean norm for a point
+func (p Point) Norm() (norm float64) {
+	norm = math.Sqrt(math.Pow(p.x, 2) + math.Pow(p.y, 2))
+	return
+}
